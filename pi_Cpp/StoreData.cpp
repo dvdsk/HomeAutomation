62bc0d7913046e061805d6f030de3de368d7cf8f
David <dskleingeld@gmail.com>
2016-10-18 21:47:39 +0200

    last commit before reshaping shit

--- pi_Cpp/StoreData.cpp
+++ pi_Cpp/StoreData.cpp
@@ -4,9 +4,108 @@ StoreData::StoreData(){
 	
 	mkdir("data", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 	//open file in binairy mode with appending write operations
-  atmospherics_file = fopen("data/atmospheric.binDat", "a+b");
-  pirs_file = fopen("data/pirs.binDat", "a+b");  
+  atmospherics_file = fopen(FILEPATH_pirs, "a+b");
+  pirs_file = fopen(FILEPATH_pirs, "a+b");  
   //TODO maybe load buffer from file?
+  
+
+  
+  //TODO try load all data in pir, get a count of successful loads
+  //fill pir from there with 1970 placeholder data, as we always want data
+  //between 2 unix timepoints and never will want data from 1970 this will 
+  //work to ignore that data, using http://www.cplusplus.com/reference/cstdio/feof/
+  
+}
+
+//TODO find out about const correctness for the int cachesize (it is const)
+
+bool StoreData::notTimePackage(unsigned char susp_time[2],  unsigned char susp_data[2]){
+  if (susp_time[0] == susp_data[0]){
+    if (susp_time[1] == susp_data[1]){ return false; }    
+  }
+  return true;
+}
+
+uint32_t StoreData::TimeInFrontOfCache(FILE* fileToCache, int cacheSize, 
+                                       unsigned char packageLenght,
+                                       uint32_t firstTime_inCache){
+  //read a package and the one before it, at the beginning of the part we will
+  //cache check if the second package is a timeP. Work backwards until you find
+  //one. (file format requirs that there will always be one)
+  
+  unsigned char susp_time_f2[2]; //stores the first 2 bytes of suspected time 
+  unsigned char susp_data_f2[2]; //and data package
+  unsigned char timePackage[4];  //complete time package
+  int n = 0;
+  
+  fseek(fileToCache, -1*(cacheSize+n*packageLenght), SEEK_END);
+  fread(susp_time_f2, 2, 1, fileToCache);
+  do {
+    std::memcpy(susp_data_f2, susp_time_f2, 2);
+    //read packages in front of sups_data, store in susp_time
+    n++;
+    fseek(fileToCache, -1*(cacheSize+n*packageLenght), SEEK_END);
+    fread(susp_time_f2, 2, 1, fileToCache);
+    
+  } while (notTimePackage(susp_time_f2, susp_data_f2));//TODO check double negative
+  
+  fread(timePackage, 4, 1, fileToCache); 
+  
+  //conversion back to full unix time  
+  firstTime_inCache = (uint32_t)timePackage[3] << 24 | //shift high part 
+                      (uint32_t)timePackage[2] << 16 | //
+                      (uint32_t)timePackage[1] << 8  | //shift and add low part
+                      (uint32_t)timePackage[0];  
+  
+  //Done with setting the initial unix time  
+  return firstTime_inCache;
+}
+
+//Load data from file to the cache for faster access in future queries   
+int StoreData::loadbuffer(unsigned char cache[], FILE* fileToCache, 
+                          int cacheSize, uint32_t& firstTime_inCache, 
+                          unsigned char packageLenght, std::string filePath){
+  
+  int oldest;//oldest element of the buffer
+//  int filled;//number of elements read successfully
+//  struct stat filestatus;
+//  int filesize;
+
+//  stat(filePath, &filestatus);//sys call for file info
+//  filesize = filestatus.st_size;
+
+//  if( filesize > cachesize ){
+//    firstTime_inCache = TimeInFrontOfCache(fileToCache, cacheSize, packageLenght,
+//                                           firstTime_inCache);
+//  }
+//  //load buffer from file, fill remaining data with unix time 0
+//  fseek(fileToCache, -1*cacheSize, SEEK_END);//TODO check  if this fails
+//  filled = fread(cache, 1, cacheSize, fileToCache);
+
+
+//  else if (filled < cacheSize) {
+//    //not enough data exists to fill the cache, set the rest of the elements to 
+//    //unix time 0
+//    oldest = filled;
+//    if(filled+8 < cacheSize) {
+//      
+//      //insert one full timepackage at the beginning of the cache
+//      cache[filled+0] = 0; //
+//      cache[filled+1] = 0; //unix time high part, in timestamp package
+//      cache[filled+2] = 0; //
+//      cache[filled+3] = 0; //high part, in timestamp package
+//    
+//      cache[filled+4] = 0; //unix time high part in normal pir package
+//      cache[filled+5] = 0; //         
+//      filled += 8;
+//    
+//      //as all data following, unil the next timestamp package will be regarded
+//      //as data from the unix year 0
+//    }
+//  }
+//  else{ oldest = filled - 4;}
+//  
+  return oldest; 
 }
 
 StoreData::~StoreData(){
@@ -38,14 +137,15 @@ void StoreData::read_plants(unsigned char data[], int line){ }
 
 PirData::PirData(StoreData& dataStorage){
   prevData[1] = 0;//0 as in no pirs measured
-  t_begin = GetMilliSec();
+  t_begin = unix_timestamp();
   Record[0] = 0;
   Record[1] = 0;
 }
 
-bool PirData::isTimeStampPackage(unsigned char susp_time[4],  unsigned char susp_data[4]){
-  if (susp_time[2] != susp_data[0]){
-    if (susp_time[3] != susp_data[1]){ return true; }    
+//takes the first 2 bytes of 2 packages and returns if the first package is a time package
+bool PirData::isTimeStampPackage(unsigned char susp_time[2],  unsigned char susp_data[2]){
+  if (susp_time[0] != susp_data[0]){
+    if (susp_time[1] != susp_data[1]){ return true; }    
   }
   return false;
 }
@@ -94,10 +194,10 @@ uint32_t PirData::getClosestTimeStamp(int lineNumber){
   } while (!isTimeStampPackage(susp_time, susp_data));//TODO check double negative
   
   //conversion back to full unix time  
-  unix_time = (uint32_t)susp_time[1] << 24 | //shift high part 
-              (uint32_t)susp_time[0] << 16 | //
-              (uint32_t)susp_time[3] << 8  | //shift and add low part
-              (uint32_t)susp_time[2];
+  unix_time = (uint32_t)susp_time[3] << 24 | //shift high part 
+              (uint32_t)susp_time[2] << 16 | //
+              (uint32_t)susp_time[1] << 8  | //shift and add low part
+              (uint32_t)susp_time[0];
   
   std::cout << "Reading timestamp: "
 		 			<< +susp_time[0] << " "
@@ -116,24 +216,16 @@ long int PirData::unix_timestamp() {
   return now;
 }
 
-long long PirData::GetMilliSec(){
-  struct timeval tp;
-  gettimeofday(&tp, NULL);
-  //get current timestamp in milliseconds
-  long long mslong = (long long) tp.tv_sec * 1000 + tp.tv_usec / 1000; 
-  return mslong;
-}
-
 
 void PirData::putTimestamp(long int timestamp){
 
   unsigned char towrite[4];
   
   //conversion to bytes
-  towrite[2] = timestamp & 0xff;         //store low part in last two bytes
-  towrite[3] = (timestamp >> 8) & 0xff;  
-  towrite[0] = (timestamp >> 16) & 0xff; //store high part in first 2 bytes
-  towrite[1] = (timestamp >> 24) & 0xff;  
+  towrite[0] = timestamp & 0xff;         //store low part in last two bytes
+  towrite[1] = (timestamp >> 8) & 0xff;  
+  towrite[2] = (timestamp >> 16) & 0xff; //store high part in first 2 bytes
+  towrite[3] = (timestamp >> 24) & 0xff;  
   
   dataStorage.write_pir(towrite);
   std::cout << "writing TIMESTAMP PIR PACKAGE: "
@@ -214,18 +306,18 @@ void PirData::combine(unsigned char B[2]){
 }
 
 void PirData::binData(unsigned char data[2]){
-  long long timepassed;
-  timepassed = GetMilliSec() - t_begin;
+  long timepassed;
+  timepassed = unix_timestamp() - t_begin;
 
   if (timepassed < PIR_DT){
-    //add movement values to pir
+    //add movementDetect values to pir
     Record[0] = Record[0] | data[0]; 
-    Record[1] = Record[1] | data[1];
+//    Record[1] = Record[1] | data[1];
   }
   else{
     //write values collected till now
     putData(Record);  
-    t_begin = GetMilliSec();
+    t_begin = unix_timestamp();
     
     //reset pir to new values
     Record[0] = 0;
@@ -243,15 +335,16 @@ void PirData::process(unsigned char data[2]){
   unsigned char newCorrect;
   
 
-  if (isNotSame(data)){
+  if (isNotSame(data)){//als de data niet hetzelfde is
       
-    combinedCorrect = prevData[1] & data[1]; 
-    newCorrect = data[1];
-    std::memcpy(prevData, data, 2); //save before notation is changed
+    combinedCorrect = prevData[1] & data[1]; //number of 'correctly' read sensors
+    newCorrect = data[1]; //new correct data
+    std::memcpy(prevData, data, 2); //save current data into prevData before we
+    //switch notations to the storage format.
 
     convertNotation(data);      
-    if (combinedCorrect > newCorrect){ //would comparing with prev data increase knowledge?
-      combine(data); //combine data with newer data overriding older data
+    if (combinedCorrect > newCorrect){ //would merging with prev data increase knowledge?
+      combine(data); //combine old data with newer data overriding older data
     }      
     binData(data); //bin on time and write when neccesairy    
   }
--- pi_Cpp/StoreData.h
+++ pi_Cpp/StoreData.h
@@ -8,7 +8,7 @@
 #include <climits> //int max etc
 #include <cstdint> //uint16_t
 
-#include <sys/stat.h> //mkdir
+#include <sys/stat.h> //mkdir and filesize
 #include <sys/time.h>
 
 /*
@@ -24,10 +24,9 @@ TIMESTAMP PIR PACKAGE:
 total length 4 bytes, used to store the full unixtime just in front of a normal 
 pir package that crosses a treshold for putting in the full time again 
   --------------------------------------
-  - time high 16 bit | time low 16 bit -
+  - time low 16 bit | time high 16 bit -
   --------------------------------------
-time is in front so we can have 2 messages with the same low time part
- after eachother
+recognised by 2 time lows after eachother
 
 
 => test if timestamp package:
@@ -69,14 +68,51 @@ class StoreData
 	  void read_pir(unsigned char data[4], int line);
 	  void read_atmospheric(unsigned char data[18], int line);
 	  void read_plants(unsigned char data[], int line);
-    
+
+    FILE* pirs_file;    
     FILE* atmospherics_file;
-    FILE* pirs_file;
+    FILE* plants_file;
   private:
-    unsigned char cache_pir[4*KB];//caches data and keeps track of the data
-    unsigned char cache_atmospheric[18*KB];//caches data and keeps track of the data
-    unsigned char cache_plants[KB];//caches data and keeps track of the data
+    const static int CACHESIZE_pir = 4000;
+    const static int CACHESIZE_atmospheric = 4000;    
+    const static int CACHESIZE_plants = 4000;    
+
+    const static std::string FILEPATH_pirs = "data/pirs.binDat"    
+    const static std::string FILEPATH_atmospheric = "data/atmospheric.binDat" 
+    const static std::string FILEPATH_plants = "data/plants.binDat"
+    
+    int oldest_pir; //indicates oldest element in cache
+    int oldest_atmospheric;
+    int oldest_plants;
+    
+    unsigned char cache_pir[CACHESIZE_pir];
+    unsigned char cache_atmospheric[CACHESIZE_atmospheric];
+    unsigned char cache_plants[CACHESIZE_plants];
+
+    uint32_t cache_firstTime_pir;
+    uint32_t cache_firstTime_atmospheric;
+    uint32_t cache_firstTime_plants;
+
+    int loadbuffer(unsigned char cache[], FILE* fileToCache, int cacheSize, 
+                   uint32_t& firstTime_inCache, unsigned char packageLenght, std::string filePath);
+    uint32_t TimeInFrontOfCache(FILE* fileToCache, int cacheSize, unsigned char packageLenght,
+                   uint32_t firstTime_inCache);
+    bool notTimePackage(unsigned char susp_time[2],  unsigned char susp_data[2]);
 };
+
+class sensorData
+{
+  public:
+      sensorData(const int CACHESIZE, ){
+        unsigned char cache[CACHESIZE];         
+      }
+      
+      FILE* filePointer;  
+
+      std::string FILEPATH = "data/pirs.binDat"         
+      int oldestInBuffer //indicates oldest element in cache      
+      uint32_t cache_firstTime;
+}
 
 //processes the data and converts to the format for storing
 class PirData
@@ -94,12 +130,11 @@ class PirData
     unsigned char prevData[2];    
     unsigned char Record[2];
 
-    long long t_begin;
+    long int t_begin;
 
     bool TimeStampSet_first;
     bool TimeStampSet_second;    
 
-    long long GetMilliSec();
 
     long int unix_timestamp();  
 
