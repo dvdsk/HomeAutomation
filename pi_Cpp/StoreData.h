330ded98ba721688790d1e71570ce67255befe8d
David <dskleingeld@gmail.com>
2016-10-30 22:23:24 +0100

    planning and documenting

--- pi_Cpp/StoreData.h
+++ pi_Cpp/StoreData.h
@@ -31,7 +31,6 @@ recognised by 2 time lows after eachother
 --------------------------------------------
  
  
- 
  ----HIGH LEVEL ACCESS (public) ----
     | storeData: storing data with a new time,*             -universal/all data
     | getting: data from specific time frames,              -universal/all data
@@ -44,104 +43,125 @@ recognised by 2 time lows after eachother
         //TODO is this needed? (might be, raspberry pi weak? implement last anyhow)
  
  ----LOW LEVEL ACCESS (private?) ----
+ -Starting up:
+    | fill cache: fills up the cache with stored data so we can access the stored
+                  data quickly. The cache is implemented transparently.           -universal/all data
+
  -Storing a new time:
-    | process: adjust formatting                                                  -data specific
+    | process:  adjust formatting                                                 -data specific
     | compress: check if it is really new data and needs to be saved              -data specific
-    | package: add all data together and add the timestamp part                   -data specific
-    the above are all data specific.the below are not.
+    | package:  add all data together and add the timestamp part                  -data specific
+  the above are all data specific. The below are not.
     | write: writes the package to both cache and file                            -universal/all data
     
-  -getting data from specific time frames
+ -getting data from specific time frames
     | searchFT: searches for the location of the two full timestamps closest to the requested
-    |           unix times                                                        -universal/all data
+    |           unix times first                                                  -universal/all data
     | searchT:  searches onwards from the timestamps found in searchFT to find the lines of the exact times
     |           returns these lines                                               -universal/all data
-
+    | getData:  given a binairy number for the wanted columns, returns a pointer to an array where it stores 
+    |           pointers to the arrays containing the requested data              -universal/all data
+                
 */
 
 #define HALFDAYSEC 43200 //numb of sec in half a day
 #define PIR_DT 1000 //number of milliseconds to bin the pir data to
 #define KB 1000 //TODO replace with const shit (snap const shit eerst)
 
-typedef std::bitset<32> package;
 
-namespace Package {
-    // Package::getTime(thePackage);
-    // Get the 16 high bits of the package as uint16_t
-    uint16_t getTime(package thePackage) {
-      return (uint16_t) (thePackage >> 16).to_ulong();
-    }
-    // Set the 16 high bits of the package
-    uint16_t setTime(uint16_t time, package thePackage) {
-      
-    }
+class Cache
+//TODO implement this so its fully transparent. wrap fwrite and fread 
+{
+  public:
+    /*sets cache pointer to the already allocated space and sets the length 
+      of the cache, Sets oldest element and cacheOldestTimestamp_ to zero*/
+    Cache(uint8_t* cache, uint8_t packageSize, int cacheLen );
+    /*copies an array to the cache, while overwriting old data, checks if the 
+      overwritten data contained a full timestamp. If so updates the oldest
+      In cache. [During startup the cache is filled and oldest timestamp 
+      automatically set]*/
+    append(uint8_t line[]);
+    /*reads the package at a single line*/
+    read(uint8_t& line[], int lineNumber);
+    /*reads all the lines from start to start+length copies them to lines[]*/
+    readSeq(uint8_t& line[], int start, int length);
+    /*removes all lines between start and lengt, then updates the entire cache
+      filling it up again from file*/
+    remove(int lineNumber, int start, int length);
+
+    uint32_t cacheOldestTimestamp_; //unix time of the oldest package in cache
+    uint32_t lineOldest_;           //line number (in packages) of the oldest package in cache
+
+  private:
+    /*pointer to cache, in the contructor we set this to an array*/
+    uint8_t* cache_;
+    /*length of cache TODO set unit (in packages? uint8_t?)*/
+	  int cacheLen_;
+    /*indicates oldest (first added) element in the cache, we will override this
+      first TODO set unit (in packages? uint8_t?)*/
+    int cacheOldest_;
+    /*size of complete data packages*/
+    uint8_t packageSize
 }
 
-//keeps track of data and cache
-class StoreData
+//keeps track of where data is located: file pointer, cacheSize, cache, 
+//filepath, and the oldest item in cache. During searches this is used to prevent
+//the search function from leaving the transparent cache while unnessesairy.
+class Data : public Cache
 {
   public:
-	  StoreData();//
-	  ~StoreData();
-	  
-	  //appends one data line to the cache and file
-	  void write_pir(unsigned char data[4]);
-	  void write_atmospheric(unsigned char data[18]);
-	  void write_plants(unsigned char data[]);
-	  
-	  //reads one line from cache or if its not in there from the file
-	  //line is the line number in the file this corrosponds to a diffrent offset for every 'type of data'
-	  void read_pir(unsigned char data[4], int line);
-	  void read_atmospheric(unsigned char data[18], int line);
-	  void read_plants(unsigned char data[], int line);
-
-    FILE* pirs_file;    
-    FILE* atmospherics_file;
-    FILE* plants_file;
-  private: //make all these functions work for any type of package
-    //on startup load the cache from the data file
-    int loadbuffer(unsigned char cache[], FILE* fileToCache, int cacheSize, 
-                   uint32_t& firstTime_inCache, unsigned char packageLenght, std::string filePath);
+    Data(std::string filePath, uint8_t* cache, uint8_t packageSize, int cacheLen) 
+    : Cache(cache, packageSize, cacheLen)
     
-    //find the timestamp corrosponding with the oldest cache line (if that line is not a timestamp in itself)
-    //this is a level of pir data specific logic on top of the transparant cache
-    uint32_t TimeInFrontOfCache(FILE* fileToCache, int cacheSize, unsigned char packageLenght,
-                   uint32_t firstTime_inCache);
+    /*gets the file pointer for setting shut down conditions*/
+    getFileP();
     
-    bool notTimePackage(unsigned char susp_time[2],  unsigned char susp_data[2]);
-};
-
-//keeps track of where data is located: file pointer, cacheSize, cache, filepath, oldest item and how old that item is
-class Data
-{
-  public:
-    Data();
-    FILE* fileP; //pointer to file
-    unsigned char* cache;
-    std::string filePath;
-		int bufferSize;
-    int oldestInBuffer; //indicates oldest element in cache (=the next we will overwrite)
-    uint32_t cache_firstTime;
+    /*writes a package to file transparently caching it*/
+    append(uint8_t line[]);
+    /*reads a single package at a given line*/
+    read(uint8_t& line[], int lineNumber);
+    /*reads all the lines including start to (excluding) start+length copies
+     them to the array lines[]*/
+    readSeq(uint8_t& line[], int start, int length);
+    /*removes all lines between start and lengt, then updates the entire cache
+      filling it up again from file*/
+    remove(int lineNumber, int start, int length);
+   
+    /*searches for the line where a timestamp is located. First asks the cache
+      if it contains the timestamp, depending on the awnser it starts searching
+      in the cache or the file. Optionally a minimum point for the search can
+      be given. [this is usefull for searching a second timestamp later timestamp]*/
+    int searchTstamp(uint32_t Tstamp, int startLine = 0);
+    
   protected:
-    int elementLength
-    FILE*
+    /*size of complete data packages*/
+    uint8_t packageSize
+    /*pointer to file, created in the constructor during opening or creation of
+      the datafile*/
+    FILE* fileP;
+    /*path to which the constructor points*/
+    std::string filePath;
 };
 
-//processes the data and converts to the format for storing
+
+
+//data specific functions and variables, inherits AllData
 class PirData : public Data
 {
   public:    
-    PirData(StoreData& dataStorage);
+    PirData(std::string filePath, uint8_t* cache, uint8_t packageSize, int cacheLen)
+    : Data(filePath, cache, packageSize, cacheLen);
+    
     uint32_t getClosestTimeStamp(int lineNumber);
-    void process(unsigned char data[2]);
+    void process(uint8_t data[2]);
 
   private:
     StoreData dataStorage;
     
     struct timeval tp;//TODO cant this be in the function?  
 
-    unsigned char prevData[2];    
-    unsigned char Record[2];
+    uint8_t prevData[2];    
+    uint8_t Record[2];
 
     long int t_begin;
 
@@ -151,17 +171,17 @@ class PirData : public Data
 
     long int unix_timestamp();  
 
-    unsigned char compress(unsigned char data);
+    uint8_t compress(uint8_t data);
 
-    bool isTimeStampPackage(unsigned char susp_time[4],  unsigned char susp_data[4]);
+    bool isTimeStampPackage(uint8_t susp_time[4],  uint8_t susp_data[4]);
 
-    bool isNotSame(unsigned char data[2]);
+    bool isNotSame(uint8_t data[2]);
 
-    void convertNotation(unsigned char B[2]);
-    void combine(unsigned char B[2]);
-    void binData(unsigned char data[2]);
+    void convertNotation(uint8_t B[2]);
+    void combine(uint8_t B[2]);
+    void binData(uint8_t data[2]);
 
-    void putData(unsigned char data[2]);  
+    void putData(uint8_t data[2]);  
     void putTimestamp(long int timestamp);        
 };
 
