174bfc774b6223ca4af240f3c16b63f4bc68730d
dskleingeld <dskleingeld@gmail.com>
2017-01-27 00:57:46 +0100

    need to recompile libmicrohttpd with ssl support, cant be done on fedora. Time to switch OS to something with a decent package librairy

--- pi_Cpp/httpServer/mainServer.cpp
+++ pi_Cpp/httpServer/mainServer.cpp
@@ -10,22 +10,6 @@
 
 const int PORT = 8888;
 
-//working
-//int answer_to_connection(void* cls,struct MHD_Connection * connection, const char * url,
-//		                     const char * method, const char * version, const char * upload_data,
-//		                     size_t * upload_data_size, void ** ptr) {
-//  
-//  const char* page  = "<html><body>Hello, browser!</body></html>";
-//  struct MHD_Response *response;
-//  int ret;
-
-//  response = MHD_create_response_from_buffer (strlen(page), (void*) page,
-//  					                                  MHD_RESPMEM_PERSISTENT);
-//  ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
-//  MHD_destroy_response(response);
-//  return ret;
-//}
-
 int print_out_key (void *cls, enum MHD_ValueKind kind, 
                    const char *key, const char *value)
 {
@@ -146,17 +130,17 @@ int main() {
     return 1;
   }
 
-  daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY | MHD_USE_SSL,
-	   		                     PORT, NULL, NULL,
-                             &answer_to_connection, NULL,
-                             MHD_OPTION_HTTPS_MEM_KEY, key_pem,
-                             MHD_OPTION_HTTPS_MEM_CERT, cert_pem,
-                             MHD_OPTION_END);
+  daemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY | MHD_USE_DEBUG,
+	   		                    PORT, NULL, NULL,
+                            &answer_to_connection, NULL,
+                            MHD_OPTION_HTTPS_MEM_KEY, key_pem,
+                            MHD_OPTION_HTTPS_MEM_CERT, cert_pem,
+                            MHD_OPTION_END);
   
   //check if the server started alright                           
   if(NULL == daemon)
     {
-      printf ("%s\n", cert_pem);
+      //printf ("%s\n", cert_pem);
       //free memory if the server crashed
       free (key_pem);
       free (cert_pem);
