76e49b3eb1b2e2d97deaadd390f941b25b656a05
David <dskleingeld@gmail.com>
2017-02-28 20:04:54 +0100

    working compress decompression

--- pi_Cpp/compression.cpp
+++ pi_Cpp/compression.cpp
@@ -2,66 +2,46 @@
 #include <iostream>
 #include <bitset>
 
-void encode(uint8_t encoded[], uint16_t toEncode, int byte_offset, 
-						int bit_offset, int length_bits){
+constexpr uint8_t mask(const int bit_offset, const int length_bits){
 
-	uint8_t unused_bits;
-	uint8_t mask;
-	//=total bits - (needed bits - (bits in first byte))
-	unused_bits = 8-(length_bits - (8-bit_offset));
-	mask = (~0);//TODO shift here doesnt work for some reason
-	mask = mask >> unused_bits;
+//	uint8_t unused_bits;
+//	uint8_t mask;
+//	//=total bits - (needed bits - (bits in first byte))
+//	unused_bits = 8-(length_bits - (8-bit_offset));
+//	mask = (~0);//TODO shift here doesnt work for some reason
+//	mask = mask >> unused_bits;
+	//implemented in single return statement below
+
+	return (~0) >> 8-(length_bits - (8-bit_offset));
+}
+
+
+void encode(uint8_t encoded[], uint16_t toEncode, const int byte_offset, 
+						const int bit_offset, const int length_bits){
 
 	encoded[byte_offset] 		|= uint8_t(toEncode << bit_offset);
-	encoded[byte_offset+1] 	|= uint8_t(toEncode >> (8-bit_offset)) & mask;
+	encoded[byte_offset+1] 	|= uint8_t(toEncode >> (8-bit_offset)) & mask(bit_offset, length_bits);
 	
-	std::cout<<+unused_bits<<"\n";
-	std::bitset<8> x(mask);
-	std::cout<<"mask: "<<x<<"\n";
-	std::cout<<"uint8_t(toEncode >> (8-bit_offset)): "<<+uint8_t(toEncode >> (8-bit_offset))<<"\n";
-	std::cout<<"encoded[byte_offset]: "<<+encoded[byte_offset+1]<<"\n";
 }
 
 uint16_t decode(uint8_t encoded[], int byte_offset, 
 						int bit_offset, int length_bits){
 
 	uint16_t decoded;
-	uint8_t unused_bits;
-	uint8_t mask;
-
-	//=total bits - (needed bits - (bits in first byte))
-	unused_bits = 8-(length_bits - (8-bit_offset));
-	mask = (~0);//TODO shift here doesnt work for some reason
-	mask = mask >> unused_bits;
-
-	std::cout<<+unused_bits<<"\n";
-	std::bitset<8> x(mask);
-	std::cout<<"mask: "<<x<<"\n";
 
 	decoded = ((uint16_t)encoded[byte_offset] >> bit_offset ) |
-						((uint16_t)(encoded[byte_offset+1] & mask) << (8-bit_offset) );
-
-//	std::cout<<"(uint16_t)(encoded[byte_offset] >> bit_offset): "<<(uint16_t)(encoded[byte_offset] >> bit_offset)<<"\n";
-//	std::cout<<"(uint16_t)(encoded[byte_offset+1] << (8-bit_offset)) & mask: "
-//	<< encoded[byte_offset+1] << (8-bit_offset)) <<"\n";
+						((uint16_t)(encoded[byte_offset+1] & mask(bit_offset, length_bits)) << (8-bit_offset) );
 
 	return decoded;
 }
 
 int main(){
 	
-	uint16_t test = 0;
+	uint16_t test1 = 210;
 	uint8_t encoded[10] = {0,0,0,0,0,0,0,0,0,0};
 
-	std::cout<<"unencoded value:"<<test<<"\n";	
-	encode(encoded, test, 2, 4, 10);		
-
-	for(int i=0; i<10; i++){
-		//std::bitset<8> x(encoded[i]);	
-		std::cout<<"raw bit encoded[i]: "<<+encoded[i]<<"\n";
-	}
-
+	std::cout<<"unencoded value:"<<test1<<"\n";	
+	encode(encoded, test1, 2, 4, 10);			
 	std::cout<<"decoded value:"<<decode(encoded, 2, 4, 10)<<"\n";
-
-
+	decode(encoded, 2, 4, 10);
 }
