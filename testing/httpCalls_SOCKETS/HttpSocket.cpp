b097ea1276f4997b5b67d0c4909062c6b51cc57a
David Kleingeld <dskleingeld@gmail.com>
2017-07-04 14:45:02 +0200

    http socket client working, except we get errors from for example example.com

--- testing/httpCalls_SOCKETS/HttpSocket.cpp
+++ testing/httpCalls_SOCKETS/HttpSocket.cpp
@@ -11,29 +11,16 @@ void PressEnterToContinue()
   do c = getchar(); while ((c != '\n') && (c != EOF));
   }
 
+
 void error(const char *msg)
 {
     perror(msg);
     exit(0);
 }
 
-HttpSocket::HttpSocket(const char* host, uint16_t port){
-  /* first what are we going to send and where are we going to send it? */
-  int portno =        80;
-  //char *host =        "api.somesite.com";
-  char *message_fmt = "POST /apikey=%s&command=%s HTTP/1.0\r\n\r\n";
 
+HttpSocket::HttpSocket(const char* host, uint16_t portno){
   struct hostent *server;
-  int bytes, sent, received, total;
-  char message[1024],response[4096];
-
-  //if (argc < 3) { puts("Parameters: <apikey> <command>"); exit(0); }
-
-  /* fill in the parameters */
-  //sprintf(message,message_fmt,argv[1],argv[2]);
-  //printf("Request:\n%s\n",message);
-
-	memcpy(message, "GET / HTTP/1.0\r\n\r\n", strlen("GET / HTTP/1.0\r\n\r\n"));
 
   /* create the socket */
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -51,86 +38,65 @@ HttpSocket::HttpSocket(const char* host, uint16_t port){
 
   /* connect the socket */
   if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
-      error("ERROR connecting");
-
+		error("ERROR connecting");
 }
 
 HttpSocket::~HttpSocket(){
   /* close the socket */
   close(sockfd);
-
 }
 
 
 void HttpSocket::send(std::string request){
   int bytes, sent, received, total;
-  char message[1024],response[4096];
-	memcpy(message, "GET / HTTP/1.0\r\n\r\n", strlen("GET / HTTP/1.0\r\n\r\n"));
+  char* response[4096];
+
 
   /* send the request */
-  total = strlen(message);
   sent = 0;
   do {
-    bytes = write(sockfd,message+sent,total-sent);
+    bytes = write(sockfd,request.c_str()+sent,request.size()-sent);
     if (bytes < 0)
         error("ERROR writing message to socket");
     if (bytes == 0)
         break;
     sent+=bytes;
-  } while (sent < total);
+  } while (sent < request.size());
+	std::cout<<"send request\n";
+
 
   /* receive the response */
   memset(response,0,sizeof(response));
   total = sizeof(response)-1;
   received = 0;
   do {
+		std::cout<<"waiting for response\n";
     bytes = read(sockfd,response+received,total-received);
-    if (bytes < 0)
-        error("ERROR reading response from socket");
-    if (bytes == 0)
-        break;
+    if (bytes < 0) std::cerr<<"ERROR reading response from socket\n";
+    if (bytes == 0){std::cout<<"done reading, breaking\n\n"; break; }
     received+=bytes;
   } while (received < total);
 
-  if (received == total)
-      error("ERROR storing complete response from socket");
+  if (received == total) 
+		std::cerr<<"ERROR storing complete response from socket\n";
+
 
   /* process response */
   printf("Response:\n%s\n",response);
 
-//	if(connect(sockfd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) == -1){
-//			std::cerr<<"could not connect to socket";
-//			return;
-//	}
-
-//	std::lock_guard<std::mutex> guard(httpSocket_mutex);
-//	write(sockfd,request.c_str(),request.size());	
+	std::cout<<"\n";
 }
 
 
 int main()
 {
 	
-	constexpr int BUFFER_SIZE = 1024;
-	char buffer[BUFFER_SIZE];
+//	HttpSocket* lampServ = new HttpSocket("192.168.1.11", 80);
+	HttpSocket* lampServ = new HttpSocket("google.com", 80);
 
-	HttpSocket* lampServ = new HttpSocket("example.com", 80);
+	//https://www.jmarshall.com/easy/http/#structure
+	lampServ->send("GET / HTTP/1.0\n\r\n\r");
 
-	std::this_thread::sleep_for(std::chrono::seconds(10));
-
-	lampServ->send("GET /\r\n");
-
-	std::this_thread::sleep_for(std::chrono::minutes(10));
-	std::cout<<"AFTER 10 MIN:\n";
-	lampServ->send("GET /\r\n");
-
-
-//	bzero(buffer, BUFFER_SIZE);
-//	while(read(lampServ->sockfd, buffer, BUFFER_SIZE - 1) != 0){
-//		fprintf(stderr, "%s", buffer);
-//		bzero(buffer, BUFFER_SIZE);
-//	}
-	
 	delete lampServ;
   return 0;
 }
