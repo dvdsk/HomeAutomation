2e45a4f6a76e670252f925e27eafa0cfd51da3e8
DeviousD <dskleingeld@gmail.com>
2017-08-03 02:16:57 +0200

    restored files damaged by crash

--- pi_Cpp/dataStorage/SlowData.cpp
+++ pi_Cpp/dataStorage/SlowData.cpp
@@ -57,6 +57,7 @@ void SlowData::process(const uint8_t raw[EncSlowArduino::LEN_ENCODED], const uin
 		std::cout<<"EncSlowArduino::LEN_ENCODED: "<<EncSlowArduino::LEN_ENCODED<<"\n";
 		memcpy(raw_extended, raw, EncSlowArduino::LEN_ENCODED);
 		std::cout<<"co2_2: "<<decode(raw_extended, EncSlowArduino::CO2, EncSlowArduino::LEN_CO2)<<"\n";
+		std::cout<<"EncSlowFile::LEN_ENCODED-EncSlowArduino::LEN_ENCODED: "<<EncSlowFile::LEN_ENCODED-EncSlowArduino::LEN_ENCODED<<"\n";
 
 		memset(raw_extended+EncSlowArduino::LEN_ENCODED, 0, 
 		       EncSlowFile::LEN_ENCODED-EncSlowArduino::LEN_ENCODED);		
