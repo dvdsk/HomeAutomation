8f9c6afb582ed229dd9d7fdd83e1b972f7c41111
David Kleingeld <dskleingeld@gmail.com>
2017-10-19 00:25:40 +0200

    started on pre compiling http code for inclusion into program binairy

---
+++ testing/CompileTimeReadIn/createStringHeader.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+
+//std::string stripHTML(std::string str){
+//	//strip comments
+//	//std::regex std_rx(R"((?<=>)\s*)");
+//	std::regex remove_comments( R"((<!--)[\s\S]*(-->))" );
+
+//	//strip comments
+//	std::string output = regex_replace(str, remove_comments, "");
+//	return output;
+//}
+
+std::string stripHTML(std::string input){
+	std::string output;
+	output.resize(input.size());
+	unsigned int strSize = input.size();
+
+	const char* str = input.c_str();
+
+	//iterate over input
+	output += str[0];
+	output += str[1];
+	output += str[2];
+	for(unsigned int i = 3; i < strSize; ++i) {
+		//in a string copy everything
+		if(str[i] == '"'){ 
+			//in a string
+			output += str[i]; i++;
+			while(str[i] != '"'){
+				output += str[i]; i++;
+			}
+			output += str[i]; //i++ is handled in the for loop
+		}
+		//strip comments
+		else if(str[i-3] == '<' && str[i-2] == '!' && str[i-1] == '-' && str[i] == '-'){
+			//remove the comment signs already put in output str
+			output.pop_back(); 
+			output.pop_back(); 
+			output.pop_back();
+			i+=3;
+			//enterd comment
+			while(!(str[i-2] == '-' && str[i-1] == '-' && str[i] == '>'))	i++;
+			//when comment is done skip to next non linefeed/space
+			i++;
+			while(str[i] == ' ' || str[i] == '\n' || str[i] == '\r' || str[i] == '\t') i++;
+			output += str[i];	
+		}
+		else if(str[i] == '>'){
+			output += str[i];
+			i++;
+			while(str[i] == ' ' || str[i] == '\n' || str[i] == '\r' || str[i] == '\t') i++;
+			output += str[i];			
+		}
+		//keep the character
+		else{
+			output += str[i];
+		}
+	}
+	return output;
+}
+
+int main(int argc, char** argv) {
+  std::string contents;
+  std::ifstream file("example.html");
+	
+	//read in the entire file
+	if(file.is_open()){
+    // get length of file:
+    file.seekg(0, std::ios::end);
+    contents.resize(file.tellg());
+    file.seekg(0, std::ios::beg);
+
+		file.read(&contents[0], contents.size());
+		file.close();
+	}
+	contents = stripHTML(contents);
+	std::cout<<contents<<"\n";
+}
