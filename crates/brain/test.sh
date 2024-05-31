edccbe910c2a8e59a0e053a1ccb7406320e8c1d2
dvdsk <noreply@davidsk.dev>
2024-05-31 15:31:42 +0200

    adds script to try ha brain without passing in all args

---
+++ crates/brain/test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SERVER="sgc"  # ssh config name or full address
+cmds="sudo systemctl stop brain"
+ssh -t sgc "$cmds"
+
+RUST_LOG=reqwest::connect=info,hyper_util=info,sled=info,trace RUST_BACKTRACE=1 cargo r -- \
+  --data-server 192.168.1.43:1235 \
+  --mpd-ip 127.0.0.1 \
+  --port 34326 \
+  --hue-bridge-ip 192.168.1.11
+
+cmds="sudo systemctl start brain"
+ssh -t sgc "$cmds"
