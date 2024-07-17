2bd7729afbe3e33dd32abbbb40d40cc5e83210b2
dvdsk <noreply@davidsk.dev>
2024-07-17 22:39:20 +0200

    add example that fetches data from last 15m

---
+++ crates/data-store/examples/last_15m.rs
@@ -0,0 +1,65 @@
+use nucleo_matcher::pattern::Pattern;
+use protocol::Reading;
+use std::net::SocketAddr;
+
+use data_store::api::Client;
+use nucleo::pattern::{AtomKind, CaseMatching, Normalization};
+use time::{Duration, OffsetDateTime};
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// the address of the store
+    #[arg(short, long, default_value_t = SocketAddr::from(([127,0,0,1], 1236)))]
+    addr: SocketAddr,
+    reading: String,
+}
+
+fn to_path(reading: &Reading) -> String {
+    let path = format!("{:?}", reading);
+    let (path, _) = path.rsplit_once('(').unwrap();
+    let path = path.replace('(', " ");
+    path
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+    let mut client = Client::connect(cli.addr).await.unwrap();
+    let readings = client.list_data().await.unwrap();
+
+    let mut matcher = nucleo_matcher::Matcher::new(nucleo_matcher::Config::DEFAULT);
+    let pattern = Pattern::new(
+        &cli.reading,
+        CaseMatching::Ignore,
+        Normalization::Smart,
+        AtomKind::Fuzzy,
+    );
+
+    let mut buf = Vec::new();
+    let best_scored = readings
+        .into_iter()
+        .map(|r| {
+            (
+                pattern.score(
+                    nucleo::Utf32Str::new(to_path(&r).as_str(), &mut buf),
+                    &mut matcher,
+                ),
+                r,
+            )
+        })
+        .max_by_key(|(score, _)| score.unwrap_or(0))
+        .unwrap()
+        .1;
+
+    println!("Showing results for: {best_scored:?}");
+    let now = OffsetDateTime::now_utc();
+    let data = client
+        .get_data(now - Duration::minutes(60), now, best_scored, 10)
+        .await
+        .unwrap();
+
+    dbg!(data);
+}
