14b29a1bb4e3a94f46269b6d5652469cecac4c1c
dvdsk <noreply@davidsk.dev>
2024-10-15 15:06:19 +0200

    refactors chart render

--- crates/sensor-tui/src/tui/readings/render/chart/labels.rs
+++ crates/sensor-tui/src/tui/readings/render/chart/labels.rs
@@ -5,12 +5,36 @@ use ratatui::layout::Rect;
 use ratatui::text::Span;
 
 use crate::time::format::progressively_more_specified;
+use crate::tui::readings::history_len::HistoryLen;
 
 use super::Bounds;
 
 const Y_LABEL_SPACING: u16 = 10;
 
-pub fn y_labels(info: &reading::Info, layout: Rect, y_bounds: Bounds) -> Vec<String> {
+pub fn y_and_title(
+    merged_y_bounds: &Vec<(reading::Info, [f64; 2])>,
+    layout: Rect,
+) -> (Vec<String>, String) {
+    let mut title = String::new();
+    let mut labels = Vec::new();
+    for (info, y_bounds) in merged_y_bounds.iter() {
+        title.push_str(&info.unit.to_string());
+        title.push(' ');
+
+        let new = y_labels(info, layout, *y_bounds);
+        if labels.is_empty() {
+            labels = new;
+        } else {
+            for (existing, new) in labels.iter_mut().zip(new) {
+                existing.push(' ');
+                existing.push_str(new.as_str());
+            }
+        }
+    }
+    (labels, title)
+}
+
+fn y_labels(info: &reading::Info, layout: Rect, y_bounds: Bounds) -> Vec<String> {
     // Characters are about twice as high as wide
     evenly_spaced_ylabels(
         layout.height / Y_LABEL_SPACING,
@@ -22,22 +46,30 @@ pub fn y_labels(info: &reading::Info, layout: Rect, y_bounds: Bounds) -> Vec<Str
 }
 
 type Labels = Vec<Span<'static>>;
-pub fn x_labels(
+pub fn x_and_title(
     layout: Rect,
     x_bounds: Bounds,
-    scale: &progressively_more_specified::FmtScale,
-) -> Labels {
+    history_len: &HistoryLen,
+) -> (Labels, String) {
     // Characters are about twice as high as wide
     let max_labels = layout.width / Y_LABEL_SPACING / 2;
     let n_labels = max_labels.max(2);
     let x_spacing = (x_bounds[1] - x_bounds[0]) / n_labels as f64;
 
-    (0..=n_labels)
+    let scale = scale(x_bounds[1]);
+    let mut labels: Labels = (0..=n_labels)
         .map(move |i| x_bounds[0] + x_spacing * i as f64)
         .rev()
-        .map(|x| fmt(x, scale))
+        .map(|x| fmt(x, &scale))
         .map(Into::into)
-        .collect()
+        .collect();
+
+    let borrowed = labels.first_mut().expect("min labels is 2");
+    let owned = std::mem::take(borrowed);
+    labels[0] = history_len.style_left_x_label(owned);
+
+    let title = format!("Time ({scale})");
+    (labels, title)
 }
 
 fn fmt(x: f64, scale: &progressively_more_specified::FmtScale) -> String {
