d5741476511245dc72c17072756d9326abe2188e
Eva van Houten <opensource@evavh.net>
2024-10-27 12:26:53 +0100

    Divide into modules

---
+++ crates/zigbee-bridge/src/lights/state.rs
@@ -0,0 +1,32 @@
+#[derive(Debug, PartialEq)]
+pub(crate) struct State {
+    pub(crate) brightness: f64,
+    pub(crate) color_temp: usize,
+    pub(crate) color_xy: (f64, f64),
+    pub(crate) on: bool,
+}
+
+impl State {
+    pub(crate) fn new() -> Self {
+        Self {
+            on: todo!(),
+            brightness: todo!(),
+            color_temp: todo!(),
+            color_xy: todo!(),
+        }
+    }
+
+    pub(crate) fn apply(&mut self, change: Option<Change>) -> State {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct Change {
+    pub(crate) friendly_name: String,
+    pub(crate) on: Option<bool>,
+    pub(crate) brightness: Option<f64>,
+    pub(crate) color_temp: Option<usize>,
+    pub(crate) color_xy: Option<(f64, f64)>,
+}
+
