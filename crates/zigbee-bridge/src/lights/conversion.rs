c403ea15b69c443a32ac990c788bf206b15daf29
Eva van Houten <opensource@evavh.net>
2024-11-02 12:23:28 +0100

    Fix clippy pedantic lints

--- crates/zigbee-bridge/src/lights/conversion.rs
+++ crates/zigbee-bridge/src/lights/conversion.rs
@@ -1,15 +1,20 @@
+#[must_use]
 pub fn mired_to_kelvin(mired: usize) -> usize {
     1_000_000 / mired
 }
 
+#[must_use]
 pub fn kelvin_to_mired(kelvin: usize) -> usize {
     1_000_000 / kelvin
 }
 
+#[must_use]
 pub fn normalize(brightness: u8) -> f64 {
-    brightness as f64 / 254.
+    f64::from(brightness) / 254.
 }
 
+#[must_use]
+#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 pub fn denormalize(brightness: f64) -> u8 {
     (brightness * 254.).round() as u8
 }
