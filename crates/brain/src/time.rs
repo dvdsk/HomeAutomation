2c2406d14a94386a9b32b8063a39b42afe26506b
Eva van Houten <opensource@evavh.net>
2024-12-01 13:34:06 +0100

    Fix critical time zone crash on startup

--- crates/brain/src/time.rs
+++ crates/brain/src/time.rs
@@ -2,7 +2,7 @@ use jiff::{tz::TimeZone, Timestamp, Zoned};
 
 // Because the pi might be set to UTC, we need to manually add the tz
 pub(crate) fn now() -> Zoned {
-    let local = TimeZone::get("Amsterdam").unwrap();
+    let local = TimeZone::get("Europe/Amsterdam").unwrap();
     Timestamp::now().to_zoned(local)
 }
 
