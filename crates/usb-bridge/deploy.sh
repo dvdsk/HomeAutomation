68294d70db55e16fca33160b7f19215b2af6171f
dvdsk <noreply@davidsk.dev>
2025-06-15 18:26:51 +0200

    usb-bridge make deploy work on fresh system

--- crates/usb-bridge/deploy.sh
+++ crates/usb-bridge/deploy.sh
@@ -2,31 +2,31 @@
 set -e
 
 BUILD_ARG=--release
-SERVER1="sgc"  # ssh config name or full address
-SERVER2="eva@192.168.1.101"  # ssh config name or full address
 NAME=usb-bridge
 
 cargo build --target=aarch64-unknown-linux-musl $BUILD_ARG
-cargo build --target=armv7-unknown-linux-musleabihf $BUILD_ARG
-rsync -vh --progress \
-  ../../target/aarch64-unknown-linux-musl/release/$NAME \
-  $SERVER1:/tmp/
-rsync -vh --progress \
-  ../../target/armv7-unknown-linux-musleabihf/release/$NAME \
-  $SERVER2:/tmp/
 
-cmds="
-sudo mv /tmp/$NAME /home/ha/$NAME
-sudo chown ha:ha /home/ha/$NAME
-sudo systemctl restart $NAME.service
-"
+for server in "atlantis"; do
+	rsync -vh --progress \
+	  ../../target/aarch64-unknown-linux-musl/release/$NAME \
+	  $server:/tmp/
+	rsync -vh --progress \
+	  $NAME.service \
+	  $server:/tmp/
+	rsync -vh --progress \
+	  $NAME.rules \
+	  $server:/tmp/
 
-ssh -t $SERVER1 "$cmds"
+	cmds="
+	sudo mv /tmp/$NAME /usr/bin/
+	sudo mv /tmp/$NAME.service /etc/systemd/system
+	sudo mv /tmp/$NAME.rules /etc/udev/rules.d
+	sudo udevadm control --reload-rules 
+	sudo udevadm trigger
+	sudo systemctl daemon-reload
+	sudo systemctl enable $NAME.service
+	sudo systemctl restart $NAME.service
+	"
 
-cmds="
-mv /tmp/$NAME /home/eva/$NAME
-sudo chown eva:eva /home/eva/$NAME
-sudo systemctl restart $NAME.service
-"
-
-ssh -t $SERVER2 "$cmds"
+	ssh -t $server "$cmds"
+done
