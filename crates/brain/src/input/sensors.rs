edeb377805e4082aea9e7e14edbb8164afc5c664
Eva van Houten <opensource@evavh.net>
2025-08-04 15:57:34 +0200

    Autoformat

--- crates/brain/src/input/sensors.rs
+++ crates/brain/src/input/sensors.rs
@@ -5,14 +5,19 @@ use tokio::sync::broadcast;
 
 use crate::controller::Event;
 
-pub async fn subscribe(event_tx: broadcast::Sender<Event>, data_server: SocketAddr) {
-    let mut sub = ReconnectingClient::new(data_server, "ha-brain".to_owned()).subscribe();
+pub async fn subscribe(
+    event_tx: broadcast::Sender<Event>,
+    data_server: SocketAddr,
+) {
+    let mut sub =
+        ReconnectingClient::new(data_server, "ha-brain".to_owned()).subscribe();
     loop {
         match sub.next().await {
             SubMessage::Reading(reading) => {
                 event_tx.send(Event::Sensor(reading)).unwrap();
             }
-            SubMessage::ErrorReport(_) | SubMessage::AffectorControlled { .. } => continue,
+            SubMessage::ErrorReport(_)
+            | SubMessage::AffectorControlled { .. } => continue,
         }
     }
 }
